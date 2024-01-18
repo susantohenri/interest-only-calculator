2cc01f5ef6fefb55df39c1055ad6f6dfe79c39b7
Henri Susanto <henry.dinus@gmail.com>
2024-01-18 11:16:25 +0700

    remove class from previous plugin

--- interest-only-calculator.php
+++ interest-only-calculator.php
@@ -17,46 +17,46 @@ function interest_only_calculator()
     ob_start();
     wp_enqueue_style('custom-styles', plugin_dir_url(__FILE__) . 'css/custom-styles.css');
     echo "
-        <div class=\"depreciationCalculator calx\">
-            <div class=\"depreciationCalculatorForm\">
+        <div class=\"interestCalculator calx\">
+            <div class=\"interestCalculatorForm\">
                 <div class=\"oneLine\">
-                    <label for=\"initial-value\">Loan Amount (Principal)</label>
+                    <label>Loan Amount (Principal)</label>
                     <div class=\"inputField\">
                         <input type=\"text\" class=\"basicInput\" id=\"field_2yof5\" name=\"item_meta[1]\" value=\"250000\" data-cell=\"D2\" data-format=\"$0,0.00\">
                     </div>
                 </div>
                 <div class=\"oneLine\">
-                    <label for=\"initial-value\">Annual Interest Rate</label>
+                    <label>Annual Interest Rate</label>
                     <div class=\"inputField\">
                     <input class=\"basicInput\" type=\"text\" id=\"field_9f93w\" name=\"item_meta[2]\" value=\"4\" data-cell=\"D3\" data-format=\"0%\">
                     </div>
                 </div>
                 <div class=\"oneLine\">
-                    <label for=\"initial-value\">Loan Term (Years)</label>
+                    <label>Loan Term (Years)</label>
                     <div class=\"inputField\">
                         <input class=\"basicInput\" type=\"text\" id=\"field_t2onv\" name=\"item_meta[3]\" value=\"30\" data-cell=\"D4\">
                     </div>
                 </div>
                 <div class=\"oneLine\">
-                    <label for=\"initial-value\">Interest-Only Period (Years)</label>
+                    <label>Interest-Only Period (Years)</label>
                     <div class=\"inputField\">
                         <input class=\"basicInput\" type=\"text\" id=\"field_d4ban\" name=\"item_meta[4]\" value=\"5\" data-cell=\"D5\">
                     </div>
                 </div>
                 <div class=\"oneLine selectbase\">
-                    <label for=\"depreciation-method\">Monthly Interest Payment</label>
+                    <label>Monthly Interest Payment</label>
                     <div class=\"inputField\">
                         <input class=\"basicInput\" type=\"text\" id=\"field_smwk0\" name=\"item_meta[6]\" value=\"\" data-cell=\"D6\" data-formula=\"D2* (D3) / 12\" data-format=\"$0,0.00\">
                     </div>
                 </div>
                 <div class=\"oneLine selectbase\">
-                    <label for=\"depreciation-method\">Total Interest Paid During Interest-Only Period</label>
+                    <label>Total Interest Paid During Interest-Only Period</label>
                     <div class=\"inputField\">
                         <input class=\"basicInput\" type=\"text\" id=\"field_4314l\" name=\"item_meta[5]\" value=\"\" data-cell=\"D7\" data-formula=\"D6* 12 * D5\" data-format=\"0,0.00\">
                     </div>
                 </div>
                 <div class=\"oneLine selectbase\">
-                    <label for=\"depreciation-method\">Total Amount Paid at End of Term</label>
+                    <label>Total Amount Paid at End of Term</label>
                     <div class=\"inputField\">
                         <input class=\"basicInput\" type=\"text\" id=\"field_55u5o\" name=\"item_meta[7]\" value=\"\" data-cell=\"D8\" data-formula=\"D2 + D7\" data-format=\"0,0.00\">
                     </div>
